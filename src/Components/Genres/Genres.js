de56c1c13209f5a67bc1b773bdb100fa935462ed
shir15a <shir95admon@gmail.com>
2021-04-04 14:16:26 +0300

    movies categories

---
+++ src/Components/Genres/Genres.js
@@ -0,0 +1,64 @@
+import axios from 'axios'
+import React, { useEffect } from 'react';
+
+import { Chip } from "@material-ui/core";
+
+const REACT_APP_API_KEY = '1d3f8a1c0198093b711a7de4dd647d9e';
+
+function Genres({ type, genres, setGenres, selectedGenres, setSelectedGenres, setPage }) {
+
+    useEffect(() => {
+        const genresData = async () => {
+            let response = await axios.get(`https://api.themoviedb.org/3/genre/${type}/list?api_key=${REACT_APP_API_KEY}&language=en-US`)
+            console.log("Genre", response.data.genres);
+            setGenres(response.data.genres)
+        };
+        genresData();
+        return () => {
+            setGenres({});
+        };
+    }, [type, setGenres]);
+
+    // get specific genre, remove it from genres array and then put it to selected genre array
+    // ...setSelectedGenres -> chosen genre
+    // selectedGenre -> new selected genre
+    const handleAdd = (selectedGenre) => {
+        console.log(selectedGenre);
+        setSelectedGenres([...selectedGenres, selectedGenre])
+        setGenres(genres.filter((genre) => genre.id !== selectedGenre.id));
+        setPage(1);
+    }
+
+    const handleRemove = (selectedGenre) => {
+        setSelectedGenres(selectedGenres.filter((selected) => selected.id !== selectedGenre.id));
+        setGenres([...genres, selectedGenre]);
+        setPage(1);
+    };
+
+    return (
+        <div>
+            {selectedGenres.map((genre) => {
+                return <Chip label={genre.name}
+                    key={genre.id}
+                    style={{ margin: 2, fontWeight: 'bold' }}
+                    clickable
+                    onDelete={() => handleRemove(genre)}
+                />
+            })}
+            {genres.map((genre) => {
+                return <Chip label={genre.name}
+                    key={genre.id}
+                    style={{ margin: 2, fontWeight: 'bold' }}
+                    clickable
+                    onClick={() => handleAdd(genre)} />
+            })}
+        </div>
+    )
+}
+
+export default Genres
+
+
+
+
+// eslint-disable-next-line
--- src/Components/Pages/Movies/Movies.js
+++ src/Components/Pages/Movies/Movies.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react';
 import SingleContent from '../../SingleContent/SingleContent';
 import CustomPagination from '../../Pagination/CustomPagination'
+import Genres from '../../Genres/Genres'
 
 const REACT_APP_API_KEY = '1d3f8a1c0198093b711a7de4dd647d9e';
 
@@ -10,19 +11,8 @@ function Movies() {
     const [page, setPage] = useState(1);
     const [content, setContent] = useState([]);
     const [numOfPages, setNumOfPages] = useState([]);
-
-    // const fetchMovies = async () => {
-    //     const response = await axios.get(
-    //         `https://api.themoviedb.org/3/discover/movie?api_key=${REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
-    //     );
-    //     setContent(response.data.results);
-    //     setNumOfPages(response.data.total_pages);
-    // };
-
-    // useEffect(() => {
-    //     fetchMovies();
-    // }, [page]);
-
+    const [selectedGenres, setSelectedGenres] = useState([]) //after select 
+    const [genres, setGenres] = useState([])
 
     useEffect(() => {
         const moviesData = async () => {
@@ -33,13 +23,19 @@ function Movies() {
         };
         moviesData();
         return () => {
-            setNumOfPages([]); 
-          };
+            setNumOfPages({});
+            // setNumOfPages([]); 
+        };
     }, [page]);
 
     return (
         <div>
             <span className="pageTitle">Movies</span>
+            <Genres type='movie' selectedGenres={selectedGenres}
+                setSelectedGenres={setSelectedGenres}
+                genres={genres}
+                setGenres={setGenres}
+                setPage={setPage} />
             <div className='trending'>
                 {content && content.map((movie) => {
                     return <SingleContent key={movie.id}
