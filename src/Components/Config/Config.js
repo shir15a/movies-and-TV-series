5155d9cf00fa829946271e7f7dcd9f1d66d2e05a
shir15a <shir95admon@gmail.com>
2021-04-08 03:04:24 +0300

    .

--- src/Components/Config/Config.js
+++ src/Components/Config/Config.js
@@ -1,10 +1,11 @@
-// pictures of cards that don't have pictures
 
 //image sizes for tmdb
 export const img_300 = "https://image.tmdb.org/t/p/w300";
 export const img_500 = "https://image.tmdb.org/t/p/w500";
 
-// contentModal and singleContent
+// pictures of cards that don't have pictures
+
+// singleContent
 export const unavailable =
   "https://westsiderc.org/wp-content/uploads/2019/08/Image-Not-Available-450x600.png";
 
